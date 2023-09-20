e942234d4b9012e2d1c9af81ca1b7f4241a77061
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-20 13:46:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function volume_sphere() {
     //Write your code here
-  
+	const but=document.getElementById("submit");
+	
+	but.addEventListener("click",(event) =>{
+		event.preventDefault();
+		const radiusInput = document.getElementById("radius");
+        const resultElement = document.getElementById("volume");
+
+            // Get the value of the radius input and convert it to a float
+            const radius = parseFloat(radiusInput.value);
+
+            if (!isNaN(radius) && radius >= 0) {
+                // Calculate the volume of the sphere
+                const volume = (4/3) * Math.PI * Math.pow(radius, 3);
+                resultElement.value = `${volume.toFixed(2)}`;
+            } else {
+                resultElement.textContent = "Invalid input. Please enter a valid non-negative number.";
+            }
+
+	})
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
